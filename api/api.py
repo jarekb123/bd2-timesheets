d13fcd81c1f1c7eddae4225fa0912111c1829ab7
Marcin <marcin919@op.pl>
2018-05-01 15:39:26 +0200

    Base -> db.Model, insert test in api.py

--- api/api.py
+++
@@ -1,8 +0,0 @@
-from my_app import api
-from flask_restplus import Resource
-
-
-@api.route('/api')
-class Api(Resource):
-    def get(self):
-        return "get /api"
--- apis/api.py
+++ apis/api.py
@@ -10,5 +10,8 @@ class TestApi(Resource):
         test_array = {"test": "test_val"}
         #result = db.engine.execute('SELECT * from Etap').fetchone()
         result = Etap.query.first()
+        etap = Etap(id=3, Nazwa_etapu='PogChamp')
+        db.session.add(etap)
+        db.session.commit()
         print(type(result))
         return jsonify(result.Nazwa_etapu)
